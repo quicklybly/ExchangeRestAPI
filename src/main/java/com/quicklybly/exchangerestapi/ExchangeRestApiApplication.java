9d806844b3b046a20983ea7f145820391f868b4d
quicklybly <quickly.tawer@gmail.com>
2023-02-23 00:52:38 +0300

    Initial commit

---
+++ src/main/java/com/quicklybly/exchangerestapi/ExchangeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.quicklybly.exchangerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quicklybly/exchangerestapi/ExchangeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quicklybly.exchangerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

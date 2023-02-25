f7bbbb331a16125b73bc7f3877c030e3b2824c14
quicklybly <quickly.tawer@gmail.com>
2023-02-25 18:06:11 +0300

    added DTO for login and registration

---
+++ src/main/java/com/quicklybly/exchangerestapi/dto/SecretKeyDTO.java
@@ -0,0 +1,14 @@
+package com.quicklybly.exchangerestapi.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SecretKeyDTO {
+    @JsonProperty("secret_key")
+    private String secretKey;
+}
---
+++ src/main/java/com/quicklybly/exchangerestapi/dto/SignUpDTO.java
@@ -0,0 +1,15 @@
+package com.quicklybly.exchangerestapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SignUpDTO {
+    private String username;
+    private String email;
+}
